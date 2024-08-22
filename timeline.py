dba167e8be4786a888436613ba33500bf908434f
sebastien.papillon@pm.me <papillos@PORT-PAPILLOS.sesam.loc>
2024-08-22 17:25:26 +0200

    parse

--- timeline.py
+++ timeline.py
@@ -3,21 +3,39 @@ import datetime
 import json
 from dateutil import parser
 import sys
+from lat_lon_parser import parse
 
 with open(sys.argv[1]) as f:
+    index=0
     locations = json.load(f)
     for location in locations["timelineObjects"] :
         if "placeVisit" in location:
             try:
+                print("visit")
                 print(str(parser.parse(location["placeVisit"]["duration"]["startTimestamp"]).strftime('%d/%m/%Y-%H:%M'))
-                +" " 
+                +" - " 
                 +str(parser.parse(location["placeVisit"]["duration"]["endTimestamp"]).strftime('%d/%m/%Y-%H:%M'))
                 +" "
                 +location["placeVisit"]["location"]["address"])
+                print()
                 
             except Exception as e:
                 print(e)
-                
+        
+        if "activitySegment" in location:
+            try:
+                print("activity")
+                print(str(parser.parse(location["activitySegment"]["duration"]["startTimestamp"]).strftime('%d/%m/%Y-%H:%M'))
+                +" - " 
+                +str(parser.parse(location["activitySegment"]["duration"]["endTimestamp"]).strftime('%d/%m/%Y-%H:%M')))
+                for point in location["activitySegment"]["waypointPath"]["waypoints"]:
+                    print("{:.6f}".format(point["latE7"]*0.0000001)+" "+"{:.6f}".format(point["lngE7"]*0.0000001))
+                    print(parse("{:.6f}".format(point["latE7"]*0.0000001)))
+            except Exception as e:
+                print("exception with "+str(e)+" at index "+str(index))
+            print()
+        
+        index+=1
 
 
 
